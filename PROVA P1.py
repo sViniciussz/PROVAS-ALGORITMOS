e7041e1fd5c2e98cc711c63e226767fd88fd4930
Vinicius Souza <166511409+sViniciussz@users.noreply.github.com>
2025-02-06 18:37:32 -0300

    Add files via upload
    #PROVA P1 - ENUNCIADO: "sendo informados pelo usuário 10 números pares, fazer um programa que calcule para cada número um par de números primos cujo soma seja igual ao próprio número Se houver, apresente ao lado de cada número par informado, o conjunto de primos que somados representam ele."

---
+++ PROVA P1.py
@@ -0,0 +1,45 @@
+'''sendo informados pelo usuario 10 numeros pares, 
+fazer um programa que calcule para cada numero um par de numeros primos
+cuja soma seja igual ao proprio numero
+Se houver, apresentar ao lado de cada numero par informado, o conjunto
+de primos que somados representem ele
+'''
+
+import os
+import time
+
+def verificar_primo(num): 
+    if num<=1:
+        return False
+    for i in range (2, int(num**0.5)+1):
+        if num %i ==0:
+            return False
+    return True
+
+def verificar_gold(n1): #verificar goldBach
+    primos=0
+    for i in range (2,n1):
+        if verificar_primo(i) and verificar_primo(n1-i):
+            primo=i
+            return primo, n1-primo
+
+
+cont=0
+while cont<10: #quantidade de vezes do numero informado
+    num=int(input("Informe um número: "))
+    if num %2==0 and num>2: #verificar se é par e >2
+        primo, primo1= verificar_gold(num)
+        if primo !=0:
+            print(f"{num} é {primo} + {primo1}")
+        else:
+            (f"Não foi possível encontrar para {num}")
+        cont+=1
+        time.sleep(3)
+        os.system("cls")
+    else:
+        print("O número informado precisa ser par e maior que 2!")
+        print()
+
+
+
+
